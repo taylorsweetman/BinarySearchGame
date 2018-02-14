69bad2378e83dadee18fd59bec38470a56aa32d6
Taylor Sweetman <taylor.sweetman@gmail.com>
2018-02-13 22:46:24 -0500

    first working version

--- Functions.java
+++ Functions.java
@@ -3,17 +3,27 @@ package binarysearch;
 import java.util.Scanner;
 
 public class Functions {
-    
-    private int maximumVal;
+
+    private int maxVal;
+    private int minVal;
+    private int initialMax;
+    private int currentMid;
+    private int numberOfTries;
 
     public Functions() {
 
     }
 
     public void inputProcess() {
-        System.out.println("I will try to guess your secret number.");
+        System.out.println("I will try to guess your secret number");
         System.out.println("What is the maximum value your number could take?");
-        maximumVal = Integer.parseInt(read());
+        initialMax = Integer.parseInt(read());
+        maxVal = initialMax;
+        System.out.println("Think of a number between 0 and " + initialMax);
+        numberOfTries = (int) (Math.log(initialMax) / Math.log(2)) + 1;
+        System.out.println("Okay got your number? I think I can guess your number in " + numberOfTries + " tries. Try and prove me wrong!");
+        loopProcess();
+
     }
 
     public String read() {
@@ -21,10 +31,30 @@ public class Functions {
         String input = reader.nextLine();
         return input;
     }
-    
-    public int findMiddle(){
-        
+
+    public void findMiddle() {
+        int newMid = (maxVal + minVal) / 2;
+        currentMid = newMid;
     }
-    
+
+    public void loopProcess() {
+        while (true) {
+            findMiddle();
+            System.out.println("Is your number greater than " + currentMid + "? [Y or N]");
+            if (rightOfMid()) {
+                minVal = currentMid + 1;
+            } else {
+                maxVal = currentMid;
+            }
+            if (minVal == maxVal) {
+                System.out.println("Your number is " + minVal + "!");
+                break;
+            }
+        }
+    }
+
+    public boolean rightOfMid() {
+        return read().equals("Y");
+    }
 
 }
